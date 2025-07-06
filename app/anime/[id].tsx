d9c86d61b4c3ae5c693a6caedc0866f81122ac44
Ravikant Munda <rxn114392@gmail.com>
2025-07-06 16:43:00 +0530

    Added custom navigation bar and routes

---
+++ app/anime/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AnimeDetail = () => {
+  return (
+    <View>
+      <Text>AnimeDetail</Text>
+    </View>
+  )
+}
+
+export default AnimeDetail
